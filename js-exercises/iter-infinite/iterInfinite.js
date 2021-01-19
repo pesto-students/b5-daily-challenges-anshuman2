361391c151301da51ad6356013fc29f3ce90ec43
anshuman2 <60535794+anshuman2@users.noreply.github.com>
2021-01-19 20:47:27 +0530

    Add files via upload

---
+++ js-exercises/iter-infinite/iterInfinite.js
@@ -0,0 +1,57 @@
+
+function* count(start , step = 1){
+  let n = start
+  while(true)
+  {
+    yield n
+    n = n + step
+  }
+}
+
+function* cycle(iter , n){
+  let iterable = iter
+  if(n === undefined)
+  {
+    while(true)
+   { 
+    for(const el of iterable)
+    {
+      yield el
+    }
+     iterable = iter
+   } 
+  }
+  else{
+    for(let i = 0;i < n;i++)
+    {
+     for(const el of iterable)
+    {
+      yield el
+    }
+     iterable = iter
+    }
+  }
+}
+
+
+function* repeat(value , n){
+  if(n === undefined){
+    while(true)
+    {
+      yield value
+    }
+  }
+  else{
+    while(n--)
+    {
+      yield value
+    }
+  }
+}
+
+
+export {
+  count,
+  cycle,
+  repeat
+};
